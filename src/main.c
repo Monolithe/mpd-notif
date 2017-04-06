1ece3fde295d469eaf8657aaf5636d31faaa920d
Monolithe <mdhordain@outlook.com>
2017-04-06 21:30:57 +0200

    deleted useless includes in main.c

--- src/main.c
+++ src/main.c
@@ -1,16 +1,5 @@
-#include <mpd/client.h>
-#include <mpd/connection.h>
-#include <mpd/status.h>
-#include <mpd/entity.h>
-#include <mpd/search.h>
-#include <mpd/tag.h>
-#include <mpd/message.h>
-
-#include <assert.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "mpd.h"
 
