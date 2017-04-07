83bb3f735b218ad2bdf7a230d4031ae8021f879b
Monolithe <mdhordain@outlook.com>
2017-04-07 17:09:07 +0200

    added help

--- src/main.c
+++ src/main.c
@@ -14,11 +14,16 @@ Compilation : gcc -std=c99 -Wall -lmpdclient -o mpd-notif main.c mpd.c
 
 #include "mpd.h"
 
+void showHelp() {
+    printf("mpd-notif is a notification client for mpd\nusage : mpd-notif [-c IP] [-p PORT]\nOptions :\n-c <IP> : set mpd server IP\n-p <PORT> : set port\n-h : show this help\n");
+}
+
 int main(int argc, char *argv[]) {
     int opt;
     char *ip = NULL;
     int port = 0;
     int ipset = 0;
+    int help = 0;
 
     while((opt = getopt(argc, argv, "c:p:h")) != -1) {
         switch(opt) {
@@ -29,12 +34,15 @@ int main(int argc, char *argv[]) {
                 port = strtol(optarg, NULL, 10); 
                 break;
             case 'h':
-                printf("mpd-notif is a notification client for mpd\n
-                        Options :\n
-                        -c <IP> : set mpd servr IP
-                        -p <PORT> : set port");
+                showHelp();
+                help = 1;
+                break;
         }
 
+    }
+
+    if(help) {
+        return 0;
     }
 
     if(ip == NULL) {
